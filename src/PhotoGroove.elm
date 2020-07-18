9bed7522b6d42ba0d7d81c33105677c030f85360
Alexey Lopatin <cosmospirat@tutanota.com>
2020-07-18 19:05:43 +0200

    new commit

--- src/PhotoGroove.elm
+++ src/PhotoGroove.elm
@@ -1,22 +1,72 @@
 module PhotoGroove exposing (main)
 
-import Html exposing (div, h1, img, text)
+import Html exposing (div, h1, img, text, Html)
 import Html.Attributes exposing (..)
+import Html.Events exposing (onClick)
+import Browser
+import Array exposing (Array)
 
+type alias Photo =
+    { url : String }
+
+type alias Model =
+    { photos : List Photo
+    , selectedUrl : String
+    }
+
+type alias Msg =
+    { description : String, data : String }
+
+
+view : Model -> Html Msg
 view model =
     div [ class "content" ]
-        [ h1 [] [ text "Photo Groove" ]
-        , div [ id "thumbnails" ]
-            [ img [ src "http://elm-in-action.com/1.jpeg" ] []
-            , img [ src "http://elm-in-action.com/2.jpeg" ] []
-            , img [ src "http://elm-in-action.com/3.jpeg" ] []
+        [ h1 [] [ text "PhotoGroove" ]
+        , div [ id "thumbnails"]
+            (List.map (viewTumbnail model.selectedUrl) model.photos)
+        , img
+            [ class "large"
+            , src (urlPrefix ++ "large/" ++ model.selectedUrl)
             ]
+            []
         ]
 
+initialModel : Model
 initialModel =
-    [ { url = "1.jpeg" }
-    , { url = "2.jpeg" }
-    , { url = "3.jpeg" }
-    ]
+    { photos =
+          [ { url = "1.jpeg" }
+          , { url = "2.jpeg" }
+          , { url = "3.jpeg" }
+          ]
+    , selectedUrl = "1.jpeg"
+    }
+
+urlPrefix : String
+urlPrefix =
+    "http://elm-in-action.com/"
+
+viewTumbnail : String -> Photo -> Html Msg
+viewTumbnail selectedUrl thumb =
+    img [ src (urlPrefix ++ thumb.url)
+        , classList [ ( "selected", selectedUrl == thumb.url ) ]
+        , onClick { description = "ClickedPhoto", data = thumb.url}
+        ]
+        []
+
+photoArray : Array Photo
+photoArray =
+    Array.fromList initialModel.photos
+
+update msg model =
+    if msg.description == "ClickedPhoto" then
+        { model | selectedUrl = msg.data}
+
+    else
+        model
+
 main =
-    view initialModel
+    Browser.sandbox
+        { init = initialModel
+        , view = view
+        , update = update
+        }
